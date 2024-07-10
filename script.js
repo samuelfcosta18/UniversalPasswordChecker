07fbabe52bf7d54d349e04f0ed1cfb1af9b1f77c
Samuel Freitas <samuelfreitas7895@gmail.com>
2024-07-10 15:24:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,19 +15,10 @@ function checkPassword() {
     label3 = document.querySelector("#label3")
     label4 = document.querySelector("#label4")
 
-    if (password.length === 0) {
-        result.value = "no password inserted"
-        console.log("no password inserted")
-        bar.classList.remove("critical")
-        bar.classList.remove("strong")
-        bar.classList.remove("safe")
-        checkbox1.removeAttribute("checked")
-        label1.classList.remove("activated")
-        label1.classList.add("deactivated")
-    }
 
     if (pattern1.test(password) == true || password.length <= 8) {
         result.style.color ="#ff0000"
+        result.style.border = "2px solid #ff0000"
         result.value = "weak"
         console.log("weak")
         //Labels turning red
@@ -54,45 +45,67 @@ function checkPassword() {
 
         bar.classList.remove("strong")
         bar.classList.remove("safe")
+        bar.classList.remove("good")
         bar.classList.add("critical")
     }
-    if (pattern2.test(password) == true) {
+    if (pattern2.test(password) == true && pattern1.test(password) == true) {
         result.style.color="#00ff00"
-        result.value = "safe"
-        console.log("safe")
+        result.style.border = "2px solid #00ff00"
+        result.value = "good"
+        console.log("good")
+    
+        bar.classList.remove("critical")
+        bar.classList.remove("strong")
+        bar.classList.remove("safe")
+        bar.classList.add("good")
+    }
+    if (pattern2.test(password) == true) {
         label2.classList.add("activated")
         label2.classList.remove("deactivated")
         checkbox2.setAttribute("checked", "true")
-
-        bar.classList.remove("critical")
-        bar.classList.remove("strong")
-        bar.classList.add("safe")
+    } else {
+        checkbox2.removeAttribute("checked")
+        label2.classList.add("deactivated")
+        label2.classList.remove("activated")
     }
-    if (pattern3.test(password) == true && password.length >= 10) {
-        result.style.color = "#00ff00"
+    if (pattern2.test(password) == true && pattern1.test(password) == true && pattern3.test(password) == true) {
+        result.style.color = "#29c429"
+        result.style.border = "2px solid #29c429"
         result.value = "safe"
         console.log("safe")
 
+        bar.classList.remove("critical")
+        bar.classList.remove("strong")
+        bar.classList.remove("good")
+        bar.classList.add("safe")
+    }
+    if (pattern3.test(password) == true) {
         checkbox4.setAttribute("checked", "true")
         label4.classList.add("activated")
         label4.classList.remove("deactivated")
-
-        bar.classList.remove("critical")
-        bar.classList.remove("strong")
-        bar.classList.add("safe")
+    } else {
+        checkbox4.removeAttribute("checked")
+        label4.classList.remove("activated")
+        label4.classList.add("deactivated")
     }
-    if (pattern4.test(password) == true && pattern3.test(password) == true && pattern2.test(password) == true) {
+    if (pattern4.test(password) == true && pattern3.test(password) == true && pattern2.test(password) == true && pattern1.test(password) == true) {
         result.style.color ="#038f00"
+        result.style.border = "2px solid #038f00"
         result.value = "strong"
         console.log("strong")
         
         bar.classList.add("strong")
         bar.classList.remove("safe")
+        bar.classList.remove("good")
         bar.classList.remove("critical")
     }
     if (pattern4.test(password) == true) {
         label3.classList.add("activated")
         label3.classList.remove("deactivated")
         checkbox3.setAttribute("checked", "true")
+    } else {
+        checkbox3.removeAttribute("checked")
+        label3.classList.remove("activated")
+        label3.classList.add("deactivated")
     }
 }
